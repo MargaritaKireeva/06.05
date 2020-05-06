74c4ac61038a85f3cb33ae379bbc2a7eee8d4df5
MargaritaKireeva <56782583+MargaritaKireeva@users.noreply.github.com>
2020-05-06 15:55:43 +0400

    Add files via upload

---
+++ 06.05 2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <fstream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	queue<char> st;
+	ifstream in("input.txt");
+	ofstream out("output.txt");
+	char x;
+	int N;
+	in >> N;
+	while (in >> x)
+	{
+		if (isdigit(x))
+			if (st.size() != N)
+				st.push(x);
+			else
+			{
+				st.pop();
+				st.push(x);
+			}
+		if (x == '*')
+		{
+			if (!st.empty())
+			{
+				out << st.front();
+				st.pop();
+			}
+			else out << x;
+		}
+
+	}
+	return 0;
+}
